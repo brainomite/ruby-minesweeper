975fd70f8f893e97d07003febfa508b6b52e8367
Aaron Young <brainomite@gmail.com>
2019-10-14 10:35:57 -0400

    Add board functionality

--- board.rb
+++ board.rb
@@ -1,8 +1,16 @@
 require_relative "tile.rb"
 
 class Board
-  def initialize(grid)
-    @grid = grid
+  def initialize()
+    @grid = Array.new(9) { Array.new }
+    @grid.each_with_index do |col, row_idx|
+      9.times { |col_idx| col << Tile.new([row_idx, col_idx], self)}
+    end
+    bombs = Board.get_ten_random_pos
+    bombs.each do |bomb|
+      x,y = bomb
+      @grid[x][y].make_bomb
+    end
   end
 
   def self.new_random_board
@@ -18,14 +26,6 @@ class Board
     Board.new(grid)
   end
 
-  def display
-    puts "  " + (0..8).to_a.join(" ")
-    @grid.each_with_index do |row, idx|
-      puts idx.to_s + " " + row.join(' ')
-    end
-    nil
-  end
-
   def self.get_ten_random_pos
     positions = []
     9.times do |x|
@@ -35,5 +35,42 @@ class Board
     end
     positions.shuffle!
     positions.take(10)
+  end
+
+  def [](pos)
+    x, y = pos
+    @grid[x][y]
+  end
+
+  def display
+    puts "  " + (0..8).to_a.join(" ")
+    @grid.each_with_index do |row, idx|
+      puts idx.to_s + " " + row.join(' ')
+    end
+    nil
+  end
+
+  def flag(pos)
+    x, y = pos
+    @grid[x][y].flag
+  end
+
+  def out_of_bounds?(pos)
+    x, y = pos
+    return true if x < 0 || y < 0 || x >= @grid.length || y >= @grid.length
+    false
+  end
+
+  def reveal(pos)
+    self[pos].reveal
+  end
+
+  def reveal_all
+    @grid.each do |row|
+      row.each do |tile|
+        tile.reveal
+      end
+    end
+    nil
   end
 end
--- tile.rb
+++ tile.rb
@@ -1,5 +1,8 @@
 require "colorize"
+# require "byebug"
+
 class Tile
+
   def initialize(pos, board)
     @pos = pos
     @board = board
@@ -9,16 +12,31 @@ class Tile
     @cheating = true
   end
 
+  def inspect
+    {
+      "pos" => @pos,
+      "revealed" => @revealed,
+      "is_bomb" => @is_bomb,
+      "flagged" => @flagged
+    }
+  end
+
   def to_s
     # @pos.to_s
-    if !@revealed
+    if @flagged
+      "F"
+    elsif !@revealed
       if @is_bomb && @cheating
         "*".colorize(:red)
       else
         "*"
       end
-    elsif @flagged
-      "f"
+    elsif is_bomb?
+      "B".colorize(:red)
+    elsif neighbor_bomb_count > 0
+      neighbor_bomb_count.to_s
+    else
+      "_"
     end
   end
 
@@ -26,20 +44,57 @@ class Tile
     @flagged = true
   end
 
+  def is_bomb?
+    @is_bomb
+  end
+
   def make_bomb
     @is_bomb = true
   end
 
   def neighbors
-    #return array of neighbor tiles
+    #return array of neighbor pos
+    x, y = @pos
+    neighbor_positions = []
+    (-1..1).each do |x_mod|
+      (-1..1).each do |y_mod|
+        next if x_mod == 0 && y_mod == 0
+        new_x = x + x_mod
+        new_y = y + y_mod
+        new_pos = [new_x, new_y]
+        neighbor_positions << new_pos unless @board.out_of_bounds?(new_pos)
+      end
+    end
+    neighbor_positions
   end
 
   def neighbor_bomb_count
     #return count of bombs in neighbors
+    neighbors.count { |neighbor| @board[neighbor].is_bomb? }
   end
 
   def reveal
     # if no neighbors with bomb run reveal on them
-    @revealed = true
+
+    @revealed = true unless @flagged
+    if neighbor_bomb_count == 0
+      neighbors.each do |pos|
+        neighbor = @board[pos]
+        neighbor.reveal unless neighbor.revealed? || neighbor.is_bomb?
+      end
+    end
+    nil
   end
+
+  def revealed?
+    @revealed
+  end
+
+  def unflag
+    @flagged = false
+  end
+
+  #private
+
+
 end
